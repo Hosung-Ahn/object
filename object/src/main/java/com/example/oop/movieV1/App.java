dde041fe4427e74f96a919a362fad667b6a9d073
Hosung <an3735297@pusan.ac.kr>
2023-11-28 17:41:37 +0900

    객체지향 프로그래밍 : 할인 서비스 테스트

---
+++ object/src/main/java/com/example/oop/movieV1/App.java
@@ -0,0 +1,26 @@
+package com.example.oop.movieV1;
+
+import java.sql.SQLOutput;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class App {
+    public static void main(String[] args) {
+        Movie avatar = new Movie("아바타",
+                Duration.ofMinutes(120),
+                Money.wons(10000),
+                new AmountDiscountPolicy(
+                        Money.wons(800),
+                        new SequenceCondition(1),
+                        new SequenceCondition(10),
+                        new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
+                        new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))
+                ));
+        Screening screening = new Screening(avatar, 2, LocalDateTime.of(2021, 7, 12, 9, 0));
+        Screening screening_sale = new Screening(avatar, 2, LocalDateTime.of(2021, 7, 12, 10, 0));
+        System.out.println(avatar.calculateMovieFee(screening));
+        System.out.println(avatar.calculateMovieFee(screening_sale));
+    }
+}
